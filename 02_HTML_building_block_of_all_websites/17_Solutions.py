44978c9988881a5a25f8a9b72057238c6afbec75
Antun <antun.jerkovic@google.com>
2022-08-03 15:02:08 +0200

    dp

--- 02_HTML_building_block_of_all_websites/17_Solutions.py
+++ 02_HTML_building_block_of_all_websites/17_Solutions.py
@@ -1,3 +1,8 @@
 # 18'19
 
 # Jako dug video za solutions. Uz njega ide zip-mapa sa resur
+# Sam sam sa prijedjenim gradivom rjesio gotovo sve, jedino se cini da animals stranica  ima jos koju zivotinju uz kroka i kanga
+
+# Brzi pregled dugog videa:
+# Kreirao mapu "zoo" u kojoj ce biti sve
+# kreirao home, animals i prices html fajlove i ometen drugim stvarima prekinuo i idem na analiyu gotovih fajlova da vidim jesam li sto zaboravio
