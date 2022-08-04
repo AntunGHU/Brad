75a53956ff7bcae90f7cfb45d996a1cae07947d7
Antun <antun.jerkovic@gmail.com>
2022-08-04 10:55:08 +0200

    dk

--- 02_HTML_building_block_of_all_websites/17_Solutions.py
+++ 02_HTML_building_block_of_all_websites/17_Solutions.py
@@ -6,4 +6,7 @@
 # Brzi pregled dugog videa:
 # Kreirao mapu "zoo" u kojoj ce biti sve
 # kreirao home, animals i prices html fajlove i ometen drugim stvarima prekinuo i idem na analiyu gotovih fajlova da vidim jesam li sto zaboravio
-# Neejer
+# Pokusaj nastvka kod kuce otkrio mi neocekivan problem - citava mapa zoo koju sam kreirao i nanjoj zasnivao kod web-stranice nije mi se prebacila na GH niti git reagira na moje promjene unutar te mape!?!?
+# Idem dalje  porediti Bradova rjesenja sa svojim
+# Prvo u Brad-a ima html-komentar kojeg jos nisam naucio a to je "<!-- .... -->
+# tagove ul, li, a sam dobro rjesio s jedinom razlikom sto nisam vidio smisla linkati stranicu na samu sebe a Brad je! Dalje otkrivam da on nije do kraja rjesio u skladu sa introom. Prekidam poredjenja!
