d92c7cbd58bb8f2259f0c14ccafa316a938accfc
Antun <antun.jerkovic@google.com>
2022-09-12 15:12:27 +0200

    dp

--- 04_Bootstrap-styling_made_quick_and_easy/09_PracticalSolutions.py
+++ 04_Bootstrap-styling_made_quick_and_easy/09_PracticalSolutions.py
@@ -1,3 +1,11 @@
 # 22'43
 
-# Linka bs u html!
+# Kreira html i Linka bs u html! dodajuci CSS dio u head i JS dio u body!
+# Dodaje ispod CSS-linka u head i link na lokalni CSS-fajl 09_product.css
+
+# Ide u Components i dodaje zeljene:
+# Navigation Bar prvi: Componets/navBar Copy-Paste i brisanje dropdown dijela koda.
+# Kopira dio koda za link tab kako bi napravio za About, Team i Contact
+# Dodaje Welcome dio "Jumbotron"
+# Dodavanje Cards-a
+# I vise nisam imao zivce! 7'30''
